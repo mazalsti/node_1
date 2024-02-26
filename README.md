1ba766def21da553cb0dbab0f45b380802409b96
Antonio Silva <toni.cpd3@gmail.com>
2024-02-26 00:44:07 -0300

    atualizando programa

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Projeto em Node
+# Projeto em Node - 1
+Projeto de feito no modulo do curso do Boniek
+
+### Pre-requisitos globais:
+'npm i -g nodemon typescript ts-node'
+
+### instalcao
+'npm install'
+
+### Para rodar o projeto.
+'npm run star-dev'
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-import { Express } from "express";
+import express,{Request, Response}from "express"; // chamada do express
+import dotenv from "dotenv"; // configurar var de ambiente
+import mustache from "mustache-express"; // chamada do template
+import path from "path"; //path e para pegar o arquivo da raiz
+import mainRouter from "./routes/index";
+
+dotenv.config();
+
+const server = express();
+
+//configuracao do mustache
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+//configurando a pasta public
+server.use(express.static(path.join(__dirname, '../public')));
+
+//routas
+server.use(mainRouter);
+server.use((req: Request,res: Response)=>{
+    res.send('pagina nao encontrada');
+});
+
+server.listen(process.env.PORT);
